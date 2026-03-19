e363ea32b226275d80e256f4d576a6d7c0079766
Dener Puhl <denerpuhl@gmail.com>
2026-03-19 03:44:20 -0300

    test

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // proxy...
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://desafio-dev-production.up.railway.app/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
