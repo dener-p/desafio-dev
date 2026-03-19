bfff3f2cd7c52783029286183bcc27c910727037
Dener Puhl <denerpuhl@gmail.com>
2026-03-19 01:58:09 -0300

    test

---
+++ api/src/index.ts
@@ -0,0 +1,42 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import awsLambdaFastify from '@fastify/aws-lambda';
+
+import fastifyCookie from '@fastify/cookie';
+import cors from '@fastify/cors';
+
+import { AppModule } from '../src/app.module';
+
+let proxy;
+
+async function bootstrap() {
+  const adapter = new FastifyAdapter();
+
+  await adapter.register(fastifyCookie);
+
+  await adapter.register(cors, {
+    origin: true, // IMPORTANT: don't hardcode localhost in prod
+    credentials: true,
+  });
+
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    adapter,
+  );
+
+  await app.init();
+
+  const instance = app.getHttpAdapter().getInstance();
+
+  return awsLambdaFastify(instance);
+}
+
+export default async function handler(req, res) {
+  if (!proxy) {
+    proxy = await bootstrap();
+  }
+  return proxy(req, res);
+}
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,36 +1,69 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { FastifyAdapter } from '@nestjs/platform-fastify';
-import { NestFastifyApplication } from '@nestjs/platform-fastify';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { cleanupOpenApiDoc } from 'nestjs-zod';
-import cors from '@fastify/cors';
 import fastifyCookie from '@fastify/cookie';
+import cors from '@fastify/cors';
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+import awsLambdaFastify from '@fastify/aws-lambda';
+
+let proxy;
 
 async function bootstrap() {
   const adapter = new FastifyAdapter();
 
   await adapter.register(fastifyCookie);
-  // register CORS on the raw fastify instance
+
   await adapter.register(cors, {
-    origin: 'http://localhost:3000',
+    origin: true, // IMPORTANT: don't hardcode localhost in prod
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'], // fix
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // add this
   });
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     adapter,
   );
-  const swaggerConfig = new DocumentBuilder()
-    .setTitle('Desafio Técnico - Backend')
-    .setDescription('')
-    .setVersion('1.0')
-    .addTag('')
-    .build();
-  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
-  const document = cleanupOpenApiDoc(swaggerDocument);
-  SwaggerModule.setup('swagger', app, document);
-  await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
+
+  await app.init();
+
+  const instance = app.getHttpAdapter().getInstance();
+
+  return awsLambdaFastify(instance);
 }
-void bootstrap();
+
+export default async function handler(req, res) {
+  if (!proxy) {
+    proxy = await bootstrap();
+  }
+  return proxy(req, res);
+}
+
+// async function bootstrap() {
+//   const adapter = new FastifyAdapter();
+//
+//   await adapter.register(fastifyCookie);
+//   // register CORS on the raw fastify instance
+//   await adapter.register(cors, {
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+//     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'], // fix
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // add this
+//   });
+//   const app = await NestFactory.create<NestFastifyApplication>(
+//     AppModule,
+//     adapter,
+//   );
+//   const swaggerConfig = new DocumentBuilder()
+//     .setTitle('Desafio Técnico - Backend')
+//     .setDescription('')
+//     .setVersion('1.0')
+//     .addTag('')
+//     .build();
+//   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+//   const document = cleanupOpenApiDoc(swaggerDocument);
+//   SwaggerModule.setup('swagger', app, document);
+//   await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
+// }
+// void bootstrap();
