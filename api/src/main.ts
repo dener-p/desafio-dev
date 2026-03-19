fa461efb08c4b0b74a8030f1e607027fdefe6a70
Dener Puhl <denerpuhl@gmail.com>
2026-03-19 02:38:05 -0300

    test

---
+++ api/src/main.ts
@@ -0,0 +1,36 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { cleanupOpenApiDoc } from 'nestjs-zod';
+import cors from '@fastify/cors';
+import fastifyCookie from '@fastify/cookie';
+
+async function bootstrap() {
+  const adapter = new FastifyAdapter();
+
+  await adapter.register(fastifyCookie);
+  // register CORS on the raw fastify instance
+  await adapter.register(cors, {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'], // fix
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // add this
+  });
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    adapter,
+  );
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Desafio Técnico - Backend')
+    .setDescription('')
+    .setVersion('1.0')
+    .addTag('')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  const document = cleanupOpenApiDoc(swaggerDocument);
+  SwaggerModule.setup('swagger', app, document);
+  await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
+}
+void bootstrap();
